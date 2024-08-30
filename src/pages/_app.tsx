da63973950e227c778ed04a0b06ebed91cff49e7
Andrew Marek <andwmarek@gmail.com>
2024-08-29 17:08:38 -0700

    remove _app

--- src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { useSyncColorMode } from '../hooks/useSyncColorMode';
-
-function MyApp({ Component, pageProps }) {
-  useSyncColorMode();
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
